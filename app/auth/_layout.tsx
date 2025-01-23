a78cb829a706eda1e7cfffdc595f28924da519e8
hissoune <khalidhissoune962@gmail.com>
2025-01-23 10:03:24 +0100

    MYV-1: Add app/auth/_layout.tsx

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,41 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function AuthLayout() {
+  const colorScheme = useColorScheme();
+
+const [isAuth,setIsAuth] = useState(true)
+  const [loaded] = useFonts({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack >
+      <Stack.Screen name="register" options={{ headerShown: true ,title:'Register' }} />
+      <Stack.Screen name="login" options={{ headerShown: true ,title:'Login' }} />
+
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
