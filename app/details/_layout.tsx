2e392a895b3e27c5a5d7779ab3d04daaa6da3317
hissoune <khalidhissoune962@gmail.com>
2025-01-28 19:14:53 +0100

    Add details section

---
+++ app/details/_layout.tsx
@@ -0,0 +1,40 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function DetailsLayout() {
+  const colorScheme = useColorScheme();
+
+const [isAuth,setIsAuth] = useState(true)
+  const [loaded] = useFonts({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack >
+      <Stack.Screen name="moviesDetails" options={{ headerShown: true  }} />
+
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
