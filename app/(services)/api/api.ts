1d399804d441c17a68bde17fd1d629e674203d37
hissoune <khalidhissoune962@gmail.com>
2025-01-28 11:20:04 +0100

    Update API service

--- app/(services)/api/api.ts
+++ app/(services)/api/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const login = async ({email,password}:{email:string,password:string}) =>{
 
 
-    const response = await axios.post(`http://192.168.8.254:8000/api/auth/login`,{
+    const response = await axios.post(`http://192.168.0.160:8000/api/auth/login`,{
         email,
         password
     });
@@ -17,7 +17,7 @@ const login = async ({email,password}:{email:string,password:string}) =>{
 const register = async ({name,email,password}:{name:string,email:string,password:string}) =>{
 
 
-        const response = await axios.post(`http://192.168.8.254:8000/api/auth/register`,{
+        const response = await axios.post(`http://192.168.8.160:8000/api/auth/register`,{
             name,
             email,
             password
