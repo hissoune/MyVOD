b3fb77e03e451ed7977399af5106b3010cb9f174
hissoune <khalidhissoune962@gmail.com>
2025-01-29 16:24:05 +0100

    Add movies slice

---
+++ app/(redux)/moviesSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { AddToFavorite, getMovies } from "@/app/(services)/api/moviesapi";
+
+export interface Movie {
+  id: number;
+  title: string;
+  description: string;
+  releaseDate: string;
+}
+
+interface MoviesState {
+  movies: Movie[];
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+}
+
+const initialState: MoviesState = {
+  movies: [],
+  status: "idle",
+  error: null,
+};
+
+export const fetchMovies = createAsyncThunk<Movie[]>(
+  "movies/fetchMovies",
+  async () => {
+    const movies = await getMovies();
+    return movies;
+  }
+);
+
+
+const moviesSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovies.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMovies.fulfilled, (state, action: PayloadAction<Movie[]>) => {
+        state.status = "succeeded";
+        state.movies = action.payload;
+      })
+      .addCase(fetchMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message || "Failed to fetch movies";
+      })
+      
+  },
+});
+
+export const moviesReducer=  moviesSlice.reducer;
