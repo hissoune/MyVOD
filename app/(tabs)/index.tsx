52273a514d00fffb333b6dbf545e12fa74b0d7b5
hissoune <khalidhissoune962@gmail.com>
2025-02-05 17:01:27 +0100

    [Movies] fix: Refactored Movies tab index for better performance

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,8 +8,7 @@ import { useSelector } from "react-redux"
 import { fetchMovies } from "../(redux)/moviesSlice"
 import { RootState } from "../(redux)/store"
 import { replaceIp } from "@/hooks/helpers"
-
-
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 
 const sessions = [
@@ -26,7 +25,10 @@ export default function LandingPage() {
   const [activeCategory, setActiveCategory] = useState("All");
   const dispatch = useAppDispatch()
   const { movies, status } = useSelector((state:RootState) => state.movies);
-
+  const latestMovie = movies && movies.length > 0 
+  ? [...movies].sort((a, b) => new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime())[0] 
+  : null;
+  
   useEffect(() => {
     dispatch(fetchMovies());
   }, [dispatch]);
@@ -41,7 +43,11 @@ export default function LandingPage() {
       ]
     );
   };
-  
+  const handlePress = (item:any) => {
+    const movieData = encodeURIComponent(JSON.stringify(item));
+    router.push(`/details/moviesDetails?movie=${movieData}`);  
+  };
+
 const renderContent = (items: any, title: string, name: string) => (
     <View style={styles.section}>
       <Text style={styles.sectionTitle}>{title}</Text>
@@ -54,7 +60,7 @@ const renderContent = (items: any, title: string, name: string) => (
           ? items.map((item: any) => (
               <TouchableOpacity key={item._id} style={styles.itemCard}>
                 <Image
-                  source={{ uri: replaceIp(item.posterImage, "192.168.8.254") }}
+                  source={{ uri: replaceIp(item.posterImage, "192.168.8.235") }}
                   style={styles.itemImage}
                 />
                 <View style={styles.itemTitleContainer}>
@@ -85,26 +91,33 @@ const renderContent = (items: any, title: string, name: string) => (
   return (
     <ScrollView style={styles.container}>
       <View style={styles.header}>
-        <TextInput style={styles.searchBar} placeholder="Search movies and series" placeholderTextColor="#999" />
         <TouchableOpacity style={styles.iconButton} onPress={handleLogout}>
-          {/* <Ionicons name="options-outline" size={24} color="#fff" /> */}
-          <Text style={styles.itemTitle}>logout</Text>
+        <AntDesign name="logout" size={24} color="#fff" />
+                  {/* <Text style={styles.itemTitle}>logout</Text> */}
         </TouchableOpacity>
       </View>
 
       <View style={styles.featuredContent}>
-        <Image
-          source={{ uri: "https://i.pinimg.com/736x/7c/bf/f8/7cbff88448f4c039850e5604fd36d08e.jpg" }}
-          style={styles.featuredImage}
-        />
-        <View style={styles.featuredOverlay}>
-          <Text style={styles.featuredTitle}>Featured: The Mandalorian</Text>
-          <TouchableOpacity style={styles.playButton}>
-            <Ionicons name="play" size={24} color="#000" />
-            <Text style={styles.playButtonText}>Play</Text>
-          </TouchableOpacity>
-        </View>
+  {latestMovie ? (
+    <>
+      <Image
+        source={{ uri: replaceIp(latestMovie.posterImage, '192.168.8.235') }}
+        style={styles.featuredImage}
+      />
+      <View style={styles.featuredOverlay}>
+        <Text style={styles.featuredTitle}>{latestMovie.title}</Text>
+        <Text style={styles.featuredTitle}>{latestMovie.duration} min</Text>
+        <TouchableOpacity style={styles.playButton} onPress={()=>handlePress(latestMovie)}>
+          <Ionicons name="play" size={24} color="#000" />
+
+          <Text style={styles.playButtonText}>show details</Text>
+        </TouchableOpacity>
       </View>
+    </>
+  ) : (
+    <ActivityIndicator size="large" color="white" />
+  )}
+</View>
 
       <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.categoriesContainer}>
         {categories.map((category) => (
