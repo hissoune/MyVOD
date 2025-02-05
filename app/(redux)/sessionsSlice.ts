bd3714b073e36a10ac65ee95b5912962c52124fc
hissoune <khalidhissoune962@gmail.com>
2025-02-05 15:05:39 +0100

    [Redux] refactor: Moved create reservation action to session slice and added reducer to update session after reservation

--- app/(redux)/sessionsSlice.ts
+++ app/(redux)/sessionsSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { AddToFavorite, getMovies } from "@/app/(services)/api/moviesapi";
 import { getSessionsForMovie } from "../(services)/api/sessionsapi";
 import { Movie } from "./moviesSlice";
+import { createReservarion } from "../(services)/api/reservarionapi";
 export interface Room {
     _id: number;
     name: string;
@@ -11,7 +12,7 @@ export interface Session {
   _id: number;
   movie: Movie;
   room: Room;
-  seats: number[];
+  seats: [{number:number,available:boolean}];
   available:boolean;
   dateTime:string;
   price:number
@@ -19,15 +20,26 @@ export interface Session {
 
 interface SessionState {
   sessions: Session[];
+  session:Session |null,
   status: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
 const initialState: SessionState = {
     sessions: [],
+    session:null,
     status: "idle",
     error: null,
 };
+
+export const createReservation = createAsyncThunk(
+  "reservations/createReservation",
+  async ({ session, seats }: { session: string; seats: number }) => {
+      const reservation = await createReservarion(session, seats);
+      
+      return reservation;
+  }
+);
 
 export const fetchSessionsForMovies = createAsyncThunk(
   "movies/fetchSessionsForMovies",
@@ -41,7 +53,12 @@ export const fetchSessionsForMovies = createAsyncThunk(
 const sessionsSlice = createSlice({
   name: "sessions",
   initialState,
-  reducers: {},
+  reducers: {
+
+    updateSession: (state, action: PayloadAction<Session>) => {
+      state.session = action.payload;
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchSessionsForMovies.pending, (state) => {
@@ -55,8 +72,22 @@ const sessionsSlice = createSlice({
         state.status = "failed";
         state.error = action.error.message || "Failed to fetch movies";
       })
+      .addCase(createReservation.pending ,(state)=>{
+        state.status = 'loading'
+    })
+    .addCase(createReservation.fulfilled, (state ,action:PayloadAction<Session>)=>{
+        
+        state.status ="succeeded",
+        state.session = action.payload
+    })
+    .addCase(createReservation.rejected, (state,action)=>{
+        state.status ="failed",
+        state.error = action.error.message || "Failed to fetch movies"
+
+    })
+    
       
   },
 });
-
+export const { updateSession } = sessionsSlice.actions;
 export const sessionsReducer=  sessionsSlice.reducer;
