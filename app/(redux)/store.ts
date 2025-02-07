9c9cbd70a5a77a09ad53f4cb6a44874f8204d866
hissoune <khalidhissoune962@gmail.com>
2025-02-07 11:03:43 +0100

    [Movies Rating] Update store configuration for rating functionality

--- app/(redux)/store.ts
+++ app/(redux)/store.ts
@@ -3,13 +3,15 @@ import { authReducer } from "./authSlice";
 import { moviesReducer } from "./moviesSlice";
 import { sessionsReducer } from "./sessionsSlice";
 import { ReservationSliceReducer } from "./reservationSlice";
+import { commentsReducer } from "./commentsSlice";
 
  const store = configureStore({
     reducer:{
             auth:authReducer,
             movies: moviesReducer,
             sessions:sessionsReducer,
-            reservations:ReservationSliceReducer
+            reservations:ReservationSliceReducer,
+            comments:commentsReducer
 
     }
 });
