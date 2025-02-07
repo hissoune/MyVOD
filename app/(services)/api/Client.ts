f90cbb9876fd1255dd25331df29470bb15cb51a7
hissoune <khalidhissoune962@gmail.com>
2025-02-07 11:03:44 +0100

    [Movies Rating] Modify Client API for movie rating integration

--- app/(services)/api/Client.ts
+++ app/(services)/api/Client.ts
@@ -4,14 +4,17 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_APPURL,
+
 });
 axiosInstance.interceptors.request.use(
  async function (config) {
+  
     const token =await AsyncStorage.getItem('token');
     
   
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
+      config.headers['Content-Type']='multipart/form-data'
     }
 
     return config;
