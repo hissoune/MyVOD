7ab79c05c3d83aaeac5e07bb80cceba0565a0589
hissoune <khalidhissoune962@gmail.com>
2025-02-08 13:54:34 +0100

    [Utils] Implement Luhn algorithm for payment validation

--- hooks/helpers.ts
+++ hooks/helpers.ts
@@ -4,3 +4,31 @@ export  const  replaceIp =(url:string, newIp:string) =>{
     });
     return updatedUrl;
 }
+
+
+
+export const isValidCardNumber = (cardNumber: string): boolean => {
+    const cleanCardNumber = cardNumber.replace(/\D/g, "")
+  
+    if (cleanCardNumber.length !== 16) {
+      return false
+    }
+  
+    let sum = 0
+    let alternate = false
+    for (let i = cleanCardNumber.length - 1; i >= 0; i--) {
+      let n = Number.parseInt(cleanCardNumber[i])
+      if (alternate) {
+        n *= 2
+        if (n > 9) {
+          n -= 9
+        }
+      }
+      sum += n
+      alternate = !alternate
+    }
+  
+    return sum % 10 === 0
+  }
+  
+  
